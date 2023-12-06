012110057f4cc682e23556ddc5f9dcaadd254d46
heycristhian <heycristhian@gmail.com>
2023-12-05 21:40:27 -0300

    Ajustado para copiar as colunas principais para o novo arquivo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
-import os
 import math
+import os
 
 import pandas as pd
 from dateutil.relativedelta import relativedelta
@@ -30,44 +30,56 @@ def add_months(key, expiration_date, row, count, months, index):
     })
 
 
-def generate_excel(months):
+def generate_main_sheets(writer):
+    vendas.to_excel(writer, sheet_name='Vendas', index=False)
+    gastos_variaveis.to_excel(writer, sheet_name='Gastos variaveis', index=False)
+    gastos_fixos.to_excel(writer, sheet_name='Gastos fixos', index=False)
+
+
+def change_column_size(writer, key, df, column_size):
+    for col_num in range(len(df.columns)):
+        writer.sheets[key].set_column(col_num, col_num, column_size)
+
+
+def copy_exists_plan_to_new_file(xls, writer, column_size):
+    for sheet_name in xls.sheet_names:
+        if sheet_name not in months:  # Excluir as planilhas existentes que não estão em months
+            df = xls.parse(sheet_name)
+            df.to_excel(writer, sheet_name=sheet_name, index=False)
+
+            change_column_size(writer, sheet_name, df, column_size)
+
+
+def add_new_plans(writer, column_size):
+    for key in months.keys():
+        df = pd.DataFrame(months[key])
+        df.to_excel(writer, sheet_name=key, index=False)
+
+        change_column_size(writer, key, df, column_size)
+
+
+def file_exists(file_path):
+    return os.path.isfile(file_path)
+
+
+def generate_excel(months, vendas, gastos_variaveis, gastos_fixos):
     # Tamanho específico que você deseja para a coluna
     column_size = 20
 
-    # Verificar se o arquivo já existe
-    if os.path.isfile(file_path):
-        # Carregar o arquivo Excel existente
+    # months['vendas'] = vendas.to_dict(orient='records')
+
+    file_path = './data/02.xlsx'
+
+    if file_exists(file_path):
         with pd.ExcelFile(file_path) as xls:
-            # Criar um Pandas Excel writer usando o XlsxWriter como motor
             with pd.ExcelWriter(file_path, engine='xlsxwriter') as writer:
-                # Copiar as planilhas existentes para o novo arquivo
-                for sheet_name in xls.sheet_names:
-                    if sheet_name not in months:  # Excluir as planilhas existentes que não estão em months
-                        df = xls.parse(sheet_name)
-                        df.to_excel(writer, sheet_name=sheet_name, index=False)
-
-                        # Ajustar a largura específica da coluna nas planilhas existentes
-                        for col_num in range(len(df.columns)):
-                            writer.sheets[sheet_name].set_column(col_num, col_num, column_size)
-
-                # Adicionar as novas planilhas
-                for key in months.keys():
-                    df = pd.DataFrame(months[key])
-                    df.to_excel(writer, sheet_name=key, index=False)
-
-                    # Ajustar a largura específica da coluna nas novas planilhas
-                    for col_num in range(len(df.columns)):
-                        writer.sheets[key].set_column(col_num, col_num, column_size)
+                generate_main_sheets(writer)
+                copy_exists_plan_to_new_file(xls, writer, column_size)
+                add_new_plans(writer, column_size)
     else:
-        # Se o arquivo não existir, criar um novo
         with pd.ExcelWriter(file_path, engine='xlsxwriter') as writer:
-            for key in months.keys():
-                df = pd.DataFrame(months[key])
-                df.to_excel(writer, sheet_name=key, index=False)
-
-                # Ajustar a largura específica da coluna nas novas planilhas
-                for col_num in range(len(df.columns)):
-                    writer.sheets[key].set_column(col_num, col_num, column_size)
+            generate_main_sheets(writer)
+            add_new_plans(writer, column_size)
 
 
 vendas, gastos_variaveis, gastos_fixos = read_excel()
@@ -95,7 +107,7 @@ for index, row in vendas.iterrows():
 
 months = dict(sorted(months.items()))
 
-generate_excel(months)
+generate_excel(months, vendas, gastos_variaveis, gastos_fixos)
 
 print()
 print(json.dumps(months, indent=2))
